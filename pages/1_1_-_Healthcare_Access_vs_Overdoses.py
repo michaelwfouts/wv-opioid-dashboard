1761a4c951beb523e17114da7290b14759389dd9
michaelwfouts <michaelwfouts@gmail.com>
2023-11-13 10:11:24 -0500

    Updated Healthcare_Access_vs_Overdoses

--- pages/1_1_-_Healthcare_Access_vs_Overdoses.py
+++ pages/1_1_-_Healthcare_Access_vs_Overdoses.py
@@ -2,11 +2,16 @@ import streamlit as st
 
 # Set the page title
 st.set_page_config(
-    page_title="Under Construction",
+    page_title="Healthcare Access vs. Overdoses",
     page_icon="🚧",
     layout="centered",
 )
 
 # Define the content of the page
 st.write("# Page Under Construction")
-st.write("This page is currently under construction. Please check back later for updates.")
+st.write("This page is currently under construction.")
+
+# Checklist
+st.write("# Working Checklist")
+st.write("✅ Data Collection")
+st.write("🚧 Visualization Construction")
