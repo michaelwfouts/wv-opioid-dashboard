af8c5d9008985add66420c6d2d477b33252958fa
sadaf <sas0004@mix.wvu.edu>
2023-11-22 15:44:27 -0500

    starting page 1

--- pages/1_1_-_Healthcare_Access_vs_Overdoses.py
+++ pages/1_1_-_Healthcare_Access_vs_Overdoses.py
@@ -1,12 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 # Set the page title
 st.set_page_config(
     page_title="Healthcare Access vs. Overdoses",
-    page_icon="🚧",
+    page_icon="🏥",
     layout="centered",
 )
 
-# Page Content
-st.write("# Page Under Construction")
-st.write("This page is currently under construction.  Please check back later for updates")
+# load data
+df_physician_person_ratio = pd.read_csv('data/WV Drug Epidemic Dataset.xlsx - Physicians Ratio (people_1 primary care physican).csv')
+df_overdose = pd.read_csv('data/WV Drug Epidemic Dataset.xlsx - Drug Mortality (Per 100,000).csv')
+
+# TODO:
+# scatterplot -- physician/person ratio x axis, drug mortality y axis
+# each point = county
+# maybe color dots red if they are considered low physician ratio? check what this number is 
